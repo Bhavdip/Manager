bcce0f85f8f2a95aa9ae4cdef12bfb02d56c3ecf
bhavdip <bhavdip.pathar@gmail.com>
2018-03-30 08:28:56 +0530

    add firbase lib

--- manager/src/App.js
+++ manager/src/App.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase';
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Provider } from 'react-redux';
@@ -5,6 +6,18 @@ import { createStore } from 'redux';
 import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyD-FiSgTRt5Q2MQOP1ptR18NPfvNSkVuxw',
+      authDomain: 'authenticationsample-12df0.firebaseapp.com',
+      databaseURL: 'https://authenticationsample-12df0.firebaseio.com',
+      projectId: 'authenticationsample-12df0',
+      storageBucket: '',
+      messagingSenderId: '292788396203'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <Provider store={createStore(reducers)}>
